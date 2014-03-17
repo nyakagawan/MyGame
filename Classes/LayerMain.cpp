e9462ae0c98aa335ee26bab88a0d1cfe632c19db
nyakagawan <isoidemasu@gmail.com>
2014-03-17 14:08:02 +0900

    LabelButtonをおいてみた

--- Classes/LayerMain.cpp
+++ Classes/LayerMain.cpp
@@ -1,6 +1,8 @@
 
 #include "LayerMain.h"
 
+USING_NS_CC;
+
 
 LayerMain::LayerMain() {
     DPRINT("call LayerMain constructor");
@@ -10,12 +12,26 @@ LayerMain::~LayerMain() {
 }
 
 bool LayerMain::init() {
-    cocos2d::log("log tet#########");
     DPRINT("call LayerMain::init");
     if(! Layer::init()) {
         DPRINT("failed Layer::init");
         return false;
     }
+    
+    Size visibleSize = Director::getInstance()->getVisibleSize();
+    Point origin = Director::getInstance()->getVisibleOrigin();
+    
+    auto label = LabelTTF::create("Hello World", "Arial", 24);
+    auto item1 = MenuItemLabel::create(label,
+                                        [](Object *obj) {
+                                            DPRINT("item1 touched");
+                                        });
+	item1->setPosition(Point(origin.x + visibleSize.width - item1->getContentSize().width/2 ,
+                                 origin.y + item1->getContentSize().height/2));
+    auto menu = Menu::create(item1, NULL);
+    menu->setPosition(Point::ZERO);
+    this->addChild(menu, 1);
+    
     return true;
 }
 
